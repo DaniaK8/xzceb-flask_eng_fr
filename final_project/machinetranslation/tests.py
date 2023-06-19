b4e8e820bc1047e8e4d003bc41afd00213699f09
DaniaK8 <daniak@skillup.online>
2023-06-19 06:03:33 -0400

    Final changes

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -6,24 +6,18 @@ class TestE2F(unittest.TestCase):
     """English to French tests"""
     def test1(self):
         # Test Hello returns Bonjour
-        self.assertEqual(english_to_french('Hello'), 'Bonjour')
+        self.assertEqual(english_to_french('hello'), 'bonjour')
         # Test Hello does not return Hello
-        self.assertNotEqual(english_to_french('Hello'), 'Hello')
-        # Test None returns empty string
-        self.assertNotEqual(english_to_french("None"), '')
-        # Test empty string returns empty string
-        self.assertNotEqual(english_to_french(0), 0)
+        self.assertNotEqual(english_to_french('hello'), 'hello')
+        
 
 class TestF2E(unittest.TestCase):
     """French to English tests"""
     def test1(self):
         # Test Bonjour returns Hello
-        self.assertEqual(french_to_english('Bonjour'), 'Hello')
+        self.assertEqual(french_to_english('bonjour'), 'hello')
         # Test Bonjour does not return Bonjour
-        self.assertNotEqual(french_to_english('Bonjour'), 'Bonjour')
-        # Test None returns empty string
-        self.assertNotEqual(french_to_english("None"), '')
-        # Test empty string returns empty string
-        self.assertNotEqual(french_to_english(0),0)
+        self.assertNotEqual(french_to_english('bonjour'), 'bonjour')
+        
 
 unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,28 +1,13 @@
 import json
 import os
-from ibm_watson import LanguageTranslatorV3
-from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-from dotenv import load_dotenv
-
-load_dotenv()
-
-apikey = os.environ['apikey']
-url = os.environ['url']
-
-authenticator = IAMAuthenticator('Hf_dj4m-zSDbhS1hY5wz2Zj3cr8kfXFE0SiNtgbxSh0G')
-language_translator = LanguageTranslatorV3(
-    version='2018-05-01',
-    authenticator=authenticator
-)
-language_translator.set_service_url('https://api.us-south.language-translator.watson.cloud.ibm.com/instances/9c4b409a-e81c-4788-8903-dd14bc8829fd')
-
+from deep_translator import MyMemoryTranslator
 def english_to_french(english_text):
-    translation = language_translator.translate(text=english_text, model_id='en-fr').get_result()
-    french_text = translation['translations'][0]['translation']
+    french_text = MyMemoryTranslator(source='en', target='fr').translate(english_text)
+    print(french_text)
     return french_text
-
-
 def french_to_english(french_text):
-    translation = language_translator.translate(text=french_text, model_id='fr-en').get_result()
-    english_text = translation['translations'][0]['translation']
+    english_text = MyMemoryTranslator(source='fr', target='en').translate(french_text)
+    print(english_text)
     return english_text
+english_to_french("hello how are you")
+french_to_english("bonjour comment allez-vous")
